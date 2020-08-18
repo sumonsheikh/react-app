58048fa0bc625bdc1b03bb54015513884c9f6894
sumonsheikh <sm.sumon117@gmail.com>
2020-08-18 23:57:36 +0600

    accessing the object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 function printNumber(){
-    for(const i =0;i<5;i++){
+    for(let i =0;i<5;i++){
         console.log(i);
     }
-    console.log(i);
+   
 }
 
 printNumber();
+
+const person={
+    name:'sumon',
+    walk(){
+        console.log("this person can talk in bangla");
+    },
+    talk(){}
+
+}
+// access object memeber
+// let pname=person.name;
+console.log(person.walk());
