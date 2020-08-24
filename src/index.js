9e126ca5f362d92343a673d2c34fa1f52b04af24
sumonsheikh <sm.sumon117@gmail.com>
2020-08-24 22:04:10 +0600

    map function with arrow function using es6 template literals

--- src/index.js
+++ src/index.js
@@ -55,5 +55,5 @@ console.log(activeJob);
 
 // map function
 const colors=['red','green','blue'];
-const items= colors.map(color=>'<li>'+ color+'</li>');
+const items= colors.map(color=> `<li>${color}</li>`);
 console.log(items);
